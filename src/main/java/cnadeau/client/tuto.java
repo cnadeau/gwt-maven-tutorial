eaf3b1863a548f5e1da4194c18764b4b717f06df
Christian Nadeau <christian.nadeau@gmail.com>
2012-11-04 11:52:58 -0500

    test formbuilder

--- src/main/java/cnadeau/client/tuto.java
+++ src/main/java/cnadeau/client/tuto.java
@@ -1,84 +1,53 @@
 package cnadeau.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.types.VerticalAlignment;
+import com.smartgwt.client.widgets.Label;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class tuto implements EntryPoint {
+public class tuto implements EntryPoint
+{
 	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
+	 * The message displayed to the user when the server cannot be reached or returns an error.
 	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
+	private static final String SERVER_ERROR = "An error occurred while " + "attempting to contact the server. Please check your network "
 			+ "connection and try again.";
 
 	/**
 	 * This is the entry point method.
 	 */
-	public void onModuleLoad() {
-		final Button sendButton = new Button("TestButton");
-		final TextBox nameField = new TextBox();
-		nameField.setText("Test Label Content");
-		final Label errorLabel = new Label();
-		errorLabel.setText("Error label content");
+	public void onModuleLoad()
+	{
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+		// Component test = null;
+		// FormsRepository formsRepository = new FormsRepository();
+		// formsRepository.lookupForms();
+		// formsRepository.lookupSearchForms();
+		//
+		// Document formDocument = formsRepository.getFormDocument("user");
+		//
+		// FormsCustomTranslation formsCustomTranslation = new FormsCustomTranslation();
+		// Document transformedDocument = formsCustomTranslation.transformDocument(formDocument, null, FormCustomActionEnum.NONE);
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
+		// FormXmlParser formXmlParser = new FormXmlParser(new ComponentFactoryImpl());
+		// Widget formWidget = formXmlParser.parseFormDocument("");
+		//
+		// RootPanel.get().add(formWidget);
+		// System.out.println("TEST!!!!!");
 
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Button click result");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Test label content:</b>"));
-		dialogVPanel.add(textToServerLabel);
-
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		sendButton.addClickHandler(new ClickHandler() {
-
-			public void onClick(ClickEvent arg0) {
-				dialogBox.center();
-				textToServerLabel.setText(nameField.getText());
-			}
-		});
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
+		Label label = new Label();
+		label.setHeight(30);
+		label.setPadding(10);
+		label.setAlign(Alignment.CENTER);
+		label.setValign(VerticalAlignment.CENTER);
+		label.setWrap(false);
+		label.setIcon("icons/16/approved.png");
+		label.setShowEdges(true);
+		label.setContents("<i>Approved</i> for release");
+		label.draw();
 
 	}
 }
